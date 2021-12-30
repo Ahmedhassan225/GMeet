74480a78a2e468b5991ac636c2c14fe5aac780a0
Ahmed Hassan <hassanahmed.ah225@gmail.com>
2021-12-30 15:38:00 +0200

    API Controller SQLite

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("API/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context; 
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GETUsers(){
+            
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<AppUser>> GETUser(int Id){
+            return await _context.Users.FindAsync(Id);
+        }
+    }
+}
