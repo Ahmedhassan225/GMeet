106eaeff0f0c5b7ae6dfeacbe0d7811f4b26bbdb
Ahmed Hassan <hassanahmed.ah225@gmail.com>
2021-12-30 23:23:53 +0200

    Client Angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,8 @@ namespace API
                options.UseSqlite (_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -51,6 +53,9 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(policy =>{
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
