6e640f8f7581fd27bf688079d3376086ee448c5a
Ahmed Hassan <hassanahmed.ah225@gmail.com>
2021-12-31 23:33:56 +0200

    navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +26,8 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
     StoreModule.forRoot({}, {}),
     BrowserAnimationsModule,
     TooltipModule.forRoot(),
-  
+    FormsModule,
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
